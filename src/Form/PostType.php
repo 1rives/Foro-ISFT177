e6ffce9e0e717bccc00c5af22666e7662fe7f251
Diegol995 <ituu.95@gmail.com>
2023-08-11 16:13:34 -0300

    Video 11 completo - Registro de Usuarios

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -24,7 +24,7 @@ class PostType extends AbstractType
                 'required' => true
             ])
             ->add('description', TextareaType::class, ['required' => true])
-            ->add('file') // Agregar FileType
+            ->add('file', FileType::class)
             //->add('creation_date', DateTimeType::class)
             ->add('url', TextType::class, ['required' => true])
             ->add('submit', SubmitType::class)
