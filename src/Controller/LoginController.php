8c5a4ce94c3c1eeb3f46a7c4898dd66b512e9a82
Rives <josegonzalorives@gmail.com>
2023-08-11 18:53:49 -0300

    Video 12 completado
    Tema del video: Autenticación de usuarios

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // obtiene error de login al encontrarse uno
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // último login ingresado por el usuario
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(AuthenticationUtils $authenticationUtils): Response
+    {
+
+    }
+}
